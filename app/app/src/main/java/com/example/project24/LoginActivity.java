f6db78b95832b0139e1aa8458ee91454a3e88acc
sjfeenstra <33319216+sjfeenstra@users.noreply.github.com>
2019-06-22 18:20:52 +0200

    validators toegevoegd aan register en login/register upgedate

--- app/app/src/main/java/com/example/project24/LoginActivity.java
+++ app/app/src/main/java/com/example/project24/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.project24;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,19 +17,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private TextInputLayout usernameText;
+    private TextInputLayout passwordText;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final Button loginButton = findViewById(R.id.loginButton);
-        final EditText usernameText = findViewById(R.id.loginUsernameText);
-        final EditText passwordText = findViewById(R.id.loginPasswordText);
+        loginButton = findViewById(R.id.loginButton);
+        usernameText = findViewById(R.id.loginUsernameText);
+        passwordText = findViewById(R.id.loginPasswordText);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ApiClient.loginAccount(getBaseContext(), usernameText.getText().toString(), passwordText.getText().toString(), new Response.Listener<JSONObject>() {
+                ApiClient.loginAccount(getBaseContext(), usernameText.getEditText().toString(), passwordText.getEditText().toString(), new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d("Login Response", response.toString());
